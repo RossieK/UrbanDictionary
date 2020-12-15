f2bb73c5c6c1c9861e8a88182d2197ea2d87dbc5
RossieK <rosi.kirova96@gmail.com>
2020-12-15 21:23:20 +0200

    Add firebase backend details

---
+++ src/firebase-init.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyApC3AP6QZDA9_GRoIIU6ZY47hkqBByazE",
+    authDomain: "urbandictionary-app.firebaseapp.com",
+    projectId: "urbandictionary-app",
+    storageBucket: "urbandictionary-app.appspot.com",
+    messagingSenderId: "818558060813",
+    appId: "1:818558060813:web:591113e61fbb4666400844"
+};
+
+firebase.initializeApp(firebaseConfig);
